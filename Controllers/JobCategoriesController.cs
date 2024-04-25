42341ffb59a8237542842276ba65ea54d206ea45
m1nkuzu <minhndgdh190046@fpt.edu.vn>
2024-04-26 00:08:53 +0700

    remove unnecessary lines

--- Controllers/JobCategoriesController.cs
+++ Controllers/JobCategoriesController.cs
@@ -53,10 +53,6 @@ namespace FPTJob.Controllers
         {
             return View();
         }
-
-        // POST: JobCategories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("JobCategoryId,JobCategoryName")] JobCategory jobCategory)
